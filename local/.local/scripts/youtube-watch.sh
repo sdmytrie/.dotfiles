ee9355023aec5926fea71ab32f7339fe96902529
Serge DMYTRIENKO <serge@dmytrienko.fr>
2022-12-08 15:23:19 +0100

    shift

---
+++ local/.local/scripts/youtube-watch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# originally by Kris Occhipinti
+# https://www.youtube.com/watch?v=FsQuGplQvrw
+
+notify-send -t 3000 "Playing Video" "$(xclip -o)";
+#mpv --ytdl-format=bestvideo+bestaudio/best --fs --speed=1 --af=rubberband=pitch-scale=0.981818181818181 "$(xclip -o)"
+mpv --ytdl-format=bestvideo+bestaudio/best --speed=1 --af=rubberband=pitch-scale=0.981818181818181 "$(xclip -o)"
